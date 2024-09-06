ca8367fd66e7093bdcdf2f144736414a62a48021
rodrigofries01 <rfries24@gmail.com>
2024-09-06 14:00:30 -0300

    create buttom and table

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
+import { Pessoa } from '../models/Pessoa';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-crud',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './crud.component.html',
   styleUrl: './crud.component.css',
 })
 export class CrudComponent {
+  //objeto formulario
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     idade: new FormControl(null, [
@@ -23,4 +26,19 @@ export class CrudComponent {
     ]),
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
+  //visibilidade botões
+  btnCadastro: boolean = true;
+
+  vetor: Pessoa[] = [];
+
+  cadastrar() {
+    //realizar cadastro
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    //limpar os campos
+    this.formulario.reset();
+
+    //exibir no console
+    console.table(this.vetor);
+  }
 }
