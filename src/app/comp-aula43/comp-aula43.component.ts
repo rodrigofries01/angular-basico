f127ccd60ea62b48b5094ed8c008a462950a9a29
rodrigofries01 <rfries24@gmail.com>
2024-09-11 15:36:23 -0300

    Visibilidade dos botões

--- src/app/comp-aula43/comp-aula43.component.ts
+++ src/app/comp-aula43/comp-aula43.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produto } from '../models/Produto';
+import { ProdutoService } from '../server/produto.service';
 
 @Component({
   selector: 'app-comp-aula43',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comp-aula43.component.html',
-  styleUrl: './comp-aula43.component.css'
+  styleUrl: './comp-aula43.component.css',
 })
 export class CompAula43Component {
+  // Vetor
+  vetor: Produto[] = [];
 
+  // visibilidade dos botoes
+  btnCadastrar: boolean = true;
+
+  // Construtor
+  constructor(private servico: ProdutoService) {}
+
+  // inicialização do componente
+  ngOnInit() {
+    this.selecionar();
+  }
+
+  // Metodo para selecionar todos os produtos
+  selecionar() {
+    this.servico.selecionar().subscribe((retorno) => {
+      this.vetor = retorno;
+    });
+  }
 }
