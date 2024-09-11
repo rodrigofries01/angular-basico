9493809a19477b21b9d7eef048031d445dfeb6c2
rodrigofries01 <rfries24@gmail.com>
2024-09-11 17:32:24 -0300

    Alterar produto

--- src/app/comp-aula43/comp-aula43.component.ts
+++ src/app/comp-aula43/comp-aula43.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Produto } from '../models/Produto';
 import { ProdutoService } from '../server/produto.service';
 
@@ -21,8 +26,8 @@ export class CompAula43Component {
   // objeto de formulario
   formulario = new FormGroup({
     id: new FormControl(null),
-    nome: new FormControl(''),
-    preco: new FormControl(null),
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    valor: new FormControl(null, [Validators.required, Validators.min(0)]),
   });
 
   // Construtor
@@ -38,5 +43,45 @@ export class CompAula43Component {
     this.servico.selecionar().subscribe((retorno) => {
       this.vetor = retorno;
     });
+  }
+
+  // Metodo para cadastrar produtos
+  cadastrar() {
+    this.servico
+      .cadastrar(this.formulario.value as Produto)
+      .subscribe((retorno) => {
+        this.vetor.push(retorno);
+        this.formulario.reset();
+      });
+  }
+
+  // Metodo para selecionar um produto especifico
+  selecionarProduto(indice: number) {
+    this.formulario.setValue({
+      id: this.vetor[indice].id,
+      nome: this.vetor[indice].nome,
+      valor: this.vetor[indice].valor,
+    });
+    this.btnCadastrar = false;
+  }
+
+  // Metodo para alterar produto
+  alterar() {
+    this.servico
+      .alterar(this.formulario.value as Produto)
+      .subscribe((retorno) => {
+        // obter o indice do objeto alterado
+        let indiceAlterado = this.vetor.findIndex((obj) => {
+          return this.formulario.value.id === obj.id;
+        });
+        // Alterar o vetor
+        this.vetor[indiceAlterado] = retorno;
+
+        // Limpar o formulario
+        this.formulario.reset();
+
+        // Visibilidade do botão
+        this.btnCadastrar = true;
+      });
   }
 }
