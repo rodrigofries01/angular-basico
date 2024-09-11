677594406a86e4b935e1f6a0402f483160f716d3
rodrigofries01 <rfries24@gmail.com>
2024-09-11 15:52:45 -0300

    FormGroup e FormControl

--- src/app/comp-aula43/comp-aula43.component.ts
+++ src/app/comp-aula43/comp-aula43.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Produto } from '../models/Produto';
 import { ProdutoService } from '../server/produto.service';
 
 @Component({
   selector: 'app-comp-aula43',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './comp-aula43.component.html',
   styleUrl: './comp-aula43.component.css',
 })
@@ -16,6 +17,13 @@ export class CompAula43Component {
 
   // visibilidade dos botoes
   btnCadastrar: boolean = true;
+
+  // objeto de formulario
+  formulario = new FormGroup({
+    id: new FormControl(null),
+    nome: new FormControl(''),
+    preco: new FormControl(null),
+  });
 
   // Construtor
   constructor(private servico: ProdutoService) {}
